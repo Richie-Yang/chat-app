bd48fbe6f246399c0b7f9fbe873a60b0f29e24f6
Richie Yang <richieyang8813@gmail.com>
2024-06-08 22:55:30 +0800

    fix socket.io middleware process logic

--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -1,14 +1,27 @@
 import { Server, Socket } from 'socket.io';
 import { AnyObject } from '../types';
-import { Next } from 'koa';
 import { tokenService } from '../services';
 import { registerChatHandler } from './chat.handler';
 
 export { init };
 
 function init(io: Server) {
-  io.engine.use(async (request: AnyObject, res: AnyObject, next: Next) => {
-    const authToken = request.headers.authorization;
+  // io.engine.use(async (request: AnyObject, res: AnyObject, next: Next) => {
+  //   const authToken = request.headers.authorization;
+  //   console.log('authorization:', authToken);
+
+  //   const user = await tokenService
+  //     .validateToken('requestId', authToken)
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+
+  //   request.user = user;
+  //   return next();
+  // });
+
+  io.use(async (socket, next) => {
+    const authToken = socket.handshake.auth.token;
     console.log('authorization:', authToken);
 
     const user = await tokenService
@@ -17,7 +30,7 @@ function init(io: Server) {
         console.log(error);
       });
 
-    request.user = user;
+    (socket.request as AnyObject).user = user;
     return next();
   });
 
